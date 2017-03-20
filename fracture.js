d8f67f96ed2cf79cb6d7e475e7003ebc747a3f39
Alan Gutierrez <alan@prettyrobots.com>
2017-03-20 11:52:54 -0500

    Implementation.

--- fracture.js
+++ fracture.js
@@ -1,1 +1,62 @@
-module.exports = 1
+var cadence = require('cadence')
+var abend = require('abend')
+var Turnstile = require('turnstile/redux')
+var fnv = require('hash.fnv')
+var Operation = require('operation/variadic')
+var coalesce = require('extant')
+
+// TODO Need to come back and convince myself that there is no way to blow the
+// stack with turnstile calling turnstiles. I know that there isn't because of
+// Cadence. A trampoline is a trampoline. I know it, but I'm not convinced.
+
+//
+function Fracture () {
+    var vargs = Array.prototype.slice.call(arguments)
+    this._operation = Operation(vargs)
+    var options = vargs.shift()
+    var turnstile = coalesce(options.turnstile, {})
+    this._extractor = options.extractor
+    this._buckets = []
+    for (var i = 0, I = options.buckets; i < I; i++) {
+        this._buckets.push(new Turnstile(this, '_operate', {
+            setImmediate: false,
+            Date: coalesce(turnstile.Date, Date)
+        }))
+    }
+    this.turnstile = new Turnstile(this, '_fracture', options.turnstile)
+    this.health = this.turnstile.health
+}
+
+// TODO Note that stringify is going to depend on objet properties being in the
+// same order.
+
+//
+Fracture.prototype._fracture = cadence(function (async, envelope) {
+    var buffer = new Buffer(JSON.stringify(this._extractor.call(null, envelope.body.work)))
+    var index = fnv(0, buffer, 0, buffer.length) % this._buckets.length
+    this._buckets[index].enqueue(envelope.body.work, envelope.body.completed)
+})
+
+Fracture.prototype._operate = function (envelope, callback) {
+    this._operation.call(null, envelope, callback)
+}
+
+Fracture.prototype.enter = function (envelope) {
+    this.turnstile.enter({
+        started: envelope.started,
+        body: {
+            completed: coalesce(envelope.completed, abend),
+            work: envelope.body
+        }
+    })
+}
+
+Fracture.prototype.enqueue = function (work, callback) {
+    this.enter({ completed: callback, body: work })
+}
+
+Fracture.prototype.push = function (work) {
+    this.enter({ body: work })
+}
+
+module.exports = Fracture
--- t/fracture.t.js
+++ t/fracture.t.js
@@ -1,5 +1,43 @@
-require('proof')(1, prove)
+require('proof/redux')(2, require('cadence')(prove))
 
-function prove (assert) {
-    assert(require('..'), 'require')
+function prove (async, assert) {
+    var Fracture = require('..')
+    var expect = [{
+        envelope: {
+            module: 'turnstile',
+            method: 'enter',
+            when: 0,
+            waited: 0,
+            timedout: false,
+            body: { id: 1 }
+        },
+        message: 'push'
+    }, {
+        envelope: {
+            module: 'turnstile',
+            method: 'enter',
+            when: 0,
+            waited: 0,
+            timedout: false,
+            body: { id: 1 }
+        },
+        message: 'enqueue'
+    }]
+    var object = {
+        work: function (envelope, callback) {
+            var expected = expect.shift()
+            assert(envelope, expected.envelope, expected.message)
+            callback()
+        }
+    }
+    var fracture = new Fracture(object, 'work', {
+        extractor: function (work) { return work.id },
+        buckets: 3,
+        turnstile: {
+            Date: { now: function () { return 0 } }
+        }
+    })
+
+    fracture.push({ id: 1 })
+    fracture.enqueue({ id: 1 }, async())
 }
